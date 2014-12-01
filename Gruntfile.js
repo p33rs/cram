76ad2a2e9bd1a8cfc55d9cd063ff7656c4e8c182
p33rs <jon@jonpierce.net>
2014-12-01 02:52:27 -0500

    JS dependency packaging

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';\n'
+            },
+            vendor: {
+                src: [
+                    'bower_components/jquery/dist/jquery.min.js',
+                    'bower_components/events/src/*.js',
+                ],
+                dest: 'public/js/vendor.js'
+            },
+            dist: {
+                src: 'js/**/*.js',
+                dest: 'public/js/cram.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                mangle: true
+            },
+            dist: {
+                files: {
+                    '<%= concat.dist.dest %>': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        watch: {
+            files: ['<%= concat.vendor.src %>', '<%= concat.dist.src %>'],
+            tasks: ['concat', 'uglify']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+
+};
