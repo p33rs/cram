c30893d450c54b50ba6c3d6023aa9d4877b22208
p33rs <jon@jonpierce.net>
2014-12-01 02:24:41 -0500

    created routes

--- app/routes.php
+++ app/routes.php
@@ -2,25 +2,26 @@
 
 Route::pattern('id', '[0-9]+');
 
-Route::get('/', ['uses' => 'Pages@landing', 'as' => 'landing']);
-Route::get('login', ['uses' => 'Users@login']);
-Route::get('logout', ['uses' => 'Users@logout']);
-Route::get('signup', ['uses' => 'Users@signup']);
-Route::get('account', ['uses' => 'Users@account']);
-Route::get('account/save', ['uses' => 'Users@accountSave']);
-Route::get('home', ['uses' => 'Users@home']);
+Route::get('/', ['uses' => 'PagesController@landing', 'as' => 'landing']);
+Route::post('login', ['uses' => 'UsersController@login', 'as' => 'login']);
+Route::get('logout', ['uses' => 'UsersController@logout', 'as' => 'logout']);
+Route::post('signup', ['uses' => 'UsersController@signup', 'as' => 'signup']);
+Route::get('account', ['uses' => 'UsersController@account', 'as' => 'account']);
+Route::post('account/save', ['uses' => 'UsersController@accountSave', 'as' => 'account/save']);
+Route::get('photos', ['uses' => 'PhotosController@index', 'as' => 'photos']);
+Route::get('photos/new', ['uses' => 'PhotosController@new', 'as' => 'photos/new']);
 
 Route::group(['prefix' => 'user', 'before' => 'auth'], function() {
-    Route::get('{id}', ['uses' => 'Photos@index']);
-    Route::get('{id}/follow', ['uses' => 'Users@follow']);
+    Route::get('{id}', ['uses' => 'PhotosController@index', 'as' => 'user/photos']);
+    Route::post('{id}/follow', ['uses' => 'UsersController@follow', 'as' => 'user/follow']);
 });
 
 Route::group(['prefix' => 'photo', 'before' => 'auth'], function() {
-    Route::get('upload', ['uses' => 'Photos@upload']);
+    Route::get('upload', ['uses' => 'PhotosController@upload', 'as' => 'photo/upload']);
     Route::group(['prefix' => '{id}'], function() {
-        Route::get('/', ['uses' => 'Photos@view']);
-        Route::get('delete', ['uses' => 'Photos@delete']);
-        Route::get('like', ['uses' => 'Photos@like']);
+        Route::get('/', ['uses' => 'PhotosController@view', 'as' => 'photo']);
+        Route::post('delete', ['uses' => 'PhotosController@delete', 'as' => 'photo/delete']);
+        Route::post('like', ['uses' => 'PhotosController@like', 'as' => 'photo/like']);
     });
 });
 
